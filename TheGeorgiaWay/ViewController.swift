2dd82975fc2707569ccb44420d59032e6d907242
Kelsie Belan <kelsie70@gmail.com>
2017-09-12 17:01:58 -0400

    added button backgrounds with red overlay when pressed

--- TheGeorgiaWay/ViewController.swift
+++ TheGeorgiaWay/ViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bg1: UIImageView!
+    
+    @IBOutlet weak var bg2: UIImageView!
   
+    @IBOutlet weak var bg3: UIImageView!
+    
+    @IBOutlet weak var bg4: UIImageView!
+    
+    @IBOutlet weak var bg5: UIImageView!
+    
+    @IBOutlet weak var bg6: UIImageView!
+    
+    @IBOutlet weak var bg7: UIImageView!
+    
+    @IBOutlet weak var bg8: UIImageView!
+    
     @IBOutlet weak var mainScrollView: UIScrollView!
     
     @IBOutlet weak var academicButton: UIButton!
@@ -35,49 +50,56 @@ class ViewController: UIViewController {
     }
     
     @IBAction func academicButtonAction(_ sender: Any) {
+        bg1.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/academics/"
         self.navigationController?.pushViewController(vc, animated: true)
-
     }
     
     @IBAction func careerDevButtonAction(_ sender: Any) {
+        bg2.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/career-development/"
         self.navigationController?.pushViewController(vc, animated: true)
     }
    
     @IBAction func leadershipOutreachButtonAction(_ sender: Any) {
+        bg3.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/leadership/"
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func alumniButtonAction(_ sender: Any) {
+        bg4.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/career-development/former-student-athletes/"
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func communityOutreachButtonAction(_ sender: Any) {
+        bg5.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/community-outreach/"
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func personalDevButtonAction(_ sender: Any) {
+        bg6.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/events/"
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func blogButtonAction(_ sender: Any) {
+        bg7.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/news/"
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func connectButtonAction(_ sender: Any) {
+        bg8.alpha = 0.5
         let vc = showWebViewViewController()
         vc.url = "http://thegeorgiaway.com/trending-wall/"
         self.navigationController?.pushViewController(vc, animated: true)
@@ -85,7 +107,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        bg1.alpha = 1.0
+        bg2.alpha = 1.0
+        bg3.alpha = 1.0
+        bg4.alpha = 1.0
+        bg5.alpha = 1.0
+        bg6.alpha = 1.0
+        bg7.alpha = 1.0
+        bg8.alpha = 1.0
     }
 
     override func didReceiveMemoryWarning() {
